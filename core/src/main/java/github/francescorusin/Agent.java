9bc209765854e59e2255095e964c8d7ffadacbef
Francesco <rusinfrank98@gmail.com>
2024-02-12 13:25:39 +0100

    Testing.

---
+++ core/src/main/java/github/francescorusin/Agent.java
@@ -0,0 +1,4 @@
+package github.francescorusin;
+
+public interface Agent {
+}
--- core/src/main/java/github/francescorusin/Main.java
+++
@@ -1,7 +0,0 @@
-package github.francescorusin;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
