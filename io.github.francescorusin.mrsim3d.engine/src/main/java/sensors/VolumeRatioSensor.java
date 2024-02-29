5f11ea1aeb108d7dd23d0bb0918c9f31c43d80a7
Francesco Rusin <rusinfrank98@gmail.com>
2024-02-29 21:46:17 +0100

    Working on voxel controller.

--- io.github.francescorusin.mrsim3d.engine/src/main/java/sensors/VolumeRatioSensor.java
+++ io.github.francescorusin.mrsim3d.engine/src/main/java/sensors/VolumeRatioSensor.java
@@ -32,7 +32,7 @@ public final class VolumeRatioSensor implements Sensor {
   @Override
   public double[] sense(Ode4jEngine engine) {
     return new double[] {
-      (body.currentVolume() - body.minVolume()) / (body.maxVolume() - body.minVolume())
+      2 * (body.currentVolume() - body.minVolume()) / (body.maxVolume() - body.minVolume()) - 1d
     };
   }
 
