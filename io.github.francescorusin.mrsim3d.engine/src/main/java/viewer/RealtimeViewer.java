cc9852e87f0a1fb7e353463a991125783c6e8bd1
francescorusin <rusinfrank98@gmail.com>
2024-12-03 18:51:08 +0100

    A bit of progress.

--- io.github.francescorusin.mrsim3d.engine/src/main/java/viewer/RealtimeViewer.java
+++ io.github.francescorusin.mrsim3d.engine/src/main/java/viewer/RealtimeViewer.java
@@ -27,13 +27,18 @@ import org.lwjgl.opengl.GLUtil;
 import org.lwjgl.system.Callback;
 import outcome.InstantSnapshot;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
 import java.util.Objects;
 
 import static org.lwjgl.glfw.GLFW.*;
-import static org.lwjgl.opengl.GL46.*;
+import static org.lwjgl.opengl.GL33.*;
 
 public class RealtimeViewer implements Viewer {
 
@@ -116,11 +121,11 @@ public class RealtimeViewer implements Viewer {
             throw new IllegalStateException("Unable to initialize GLFW");
         }
         String title = "HAHA!";
-        int m_width = 700;
-        int m_height = 700;
+        int m_width = 800;
+        int m_height = 800;
 
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
         glfwWindowHint(GLFW_VISIBLE, GLFW_TRUE); // Sets window to be visible
         glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE); // Sets whether the window is resizable
@@ -143,40 +148,61 @@ public class RealtimeViewer implements Viewer {
         GL.createCapabilities();
         Callback debugProc = GLUtil.setupDebugMessageCallback();
 
-        final float[] chessboard_pixels = new float[64 * 64 * 4];
+        final int width = 256;
+        final int height = 256;
+        final int stepW = width / 8;
+        final int stepH = height / 8;
+        final byte[] chessboard_pixels = new byte[width * height * 4];
         int index = -1;
-        for (int i = 0; i < 64; ++i) {
-            for (int j = 0; j < 64; ++j) {
-                final float checker;
-                if ((i / 8) % 2 != (j / 8) % 2) {
-                    checker = 1f;
+        for (int i = 0; i < width; ++i) {
+            for (int j = 0; j < height; ++j) {
+                final byte checker;
+                if ((i / stepW) % 2 != (j / stepH) % 2) {
+                    checker = (byte) 255;
                 } else {
-                    checker = 1f;
+                    checker = (byte) 127;
                 }
                 chessboard_pixels[++index] = checker;
                 chessboard_pixels[++index] = checker;
                 chessboard_pixels[++index] = checker;
-                chessboard_pixels[++index] = 1.0f;
+                chessboard_pixels[++index] = (byte) 255;
             }
         }
+
+        /*BufferedImage ruby = ImageIO.read(new FileInputStream("/home/francescorusin/Downloads/Ruby.jpg"));
+        final int width = ruby.getWidth();
+        final int height = ruby.getHeight();
+        final int[] rubyPixels = new int[width * height];
+        ruby.getRGB(0, 0, width, height, rubyPixels, 0, width);
+        ByteBuffer rubyBuffer = BufferUtils.createByteBuffer(width * height * 4);
+        for (int i = 0; i < width; ++i) {
+            for (int j = 0; j < height / 2; ++j) {
+                final int pos1 = j * width + i;
+                final int pos2 = (height - j - 1) * width + i;
+                final int placeholder = rubyPixels[pos1];
+                rubyPixels[pos1] = rubyPixels[pos2];
+                rubyPixels[pos2] = placeholder;
+            }
+        }
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                int pixel = rubyPixels[y * width + x];
+                rubyBuffer.put((byte) ((pixel >> 16) & 0xFF));
+                rubyBuffer.put((byte) ((pixel >> 8) & 0xFF));
+                rubyBuffer.put((byte) (pixel & 0xFF));
+                rubyBuffer.put((byte) ((pixel >> 24) & 0xFF));
+            }
+        }*/
+
         final int texture = glGenTextures();
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, texture);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-        glPixelStorei(GL_UNPACK_ALIGNMENT, 2);
-        glTexImage2D(
-                GL_TEXTURE_2D,
-                0,
-                GL_RGBA8,
-                64,
-                64,
-                0,
-                GL_RGBA,
-                GL_FLOAT,
-                BufferUtils.createFloatBuffer(64 * 64 * 4).put(chessboard_pixels).flip());
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE,
+                BufferUtils.createByteBuffer(chessboard_pixels.length).put(chessboard_pixels).flip());
 
         final int shader =
                 createShader(
@@ -192,23 +218,31 @@ public class RealtimeViewer implements Viewer {
 
         final float[] vertexData =
                 new float[]{
-                        -.5f, -.5f, 0.0f, 0.0f,
-                        -.5f, .5f, 0.0f, 1.0f,
-                        .5f, -.5f, 1.0f, 0.0f,
-                        .5f, .5f, 1.0f, 1.0f
+                        -.75f, -.75f, 0.0f, 0.0f,
+                        -.75f, .75f, 0.0f, 1.0f,
+                        .75f, -.75f, 1.0f, 0.0f,
+                        .75f, .75f, 1.0f, 1.0f
+                };
+        final int[] indexData =
+                new int[] {
+                        0, 1, 2,
+                        1, 2, 3
                 };
         final int vertexBuffer = glGenBuffers();
         glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
         glBufferData(GL_ARRAY_BUFFER, vertexData, GL_STATIC_DRAW);
         glEnableVertexAttribArray(0);
         glVertexAttribPointer(0, 2, GL_FLOAT, false, 16, BufferUtils.createFloatBuffer(0));
+        glEnableVertexAttribArray(1);
+        glVertexAttribPointer(1, 2, GL_FLOAT, false, 16, BufferUtils.createFloatBuffer(0));
 
         while (!glfwWindowShouldClose(windowID)) {
             checkErrors();
             glClearColor(0f, 0f, 0f, 1f);
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-            glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+            glDrawArrays(GL_TRIANGLES, 0, 3);
+            glDrawArrays(GL_TRIANGLES, 1, 4);
 
             glfwSwapBuffers(windowID);
             glfwPollEvents();
