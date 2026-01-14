d44fffdbc704e1b90551bc24b984df58164e3f4f
Francesco <rusinfrank98@gmail.com>
2026-01-14 17:34:23 +0100

    Pair did not use commas when it should have.

--- io.github.francescorusin.mrsim3d.utils/src/main/java/utils/Pair.java
+++ io.github.francescorusin.mrsim3d.utils/src/main/java/utils/Pair.java
@@ -22,6 +22,6 @@ package utils;
 public record Pair<F, S>(F first, S second) {
   @Override
   public String toString() {
-    return String.format("[%s %s]", first.toString(), second.toString());
+    return String.format("[%s, %s]", first.toString(), second.toString());
   }
 }
